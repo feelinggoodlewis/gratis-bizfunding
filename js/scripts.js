d617cb883a88a555a3df9ae9a0c339d6cf85cc24
increase21 <engr.increase@gmail.com>
2019-10-04 21:18:06 +0100

    modification of the page to tell the brand

--- js/scripts.js
+++ js/scripts.js
@@ -253,6 +253,23 @@
    //    }
    // });
 
+   $("#content-slider").lightSlider({
+      loop: true,
+      keyPress: true
+   });
+   $('#image-gallery').lightSlider({
+      gallery: true,
+      item: 1,
+      thumbItem: 9,
+      slideMargin: 0,
+      speed: 500,
+      auto: true,
+      loop: true,
+      onSliderLoad: function () {
+         $('#image-gallery').removeClass('cS-hidden');
+      }
+   });
+
 
    //Gallery Slider ================================================================================
 
